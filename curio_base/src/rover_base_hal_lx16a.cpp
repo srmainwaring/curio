7b5ed0dede7dfbcee627121afb2215ae9b0f3062
Rhys Mainwaring <rhys.mainwaring@me.com>
2020-05-05 14:49:47 +0100

    Load parameters for the L16A rover base hardware.
    1. Use runtime parameters to set servo ids, location and orientation.
    2. Move curio_bringup back to the top level (revert unintended move).

--- curio_base/src/rover_base_hal_lx16a.cpp
+++ curio_base/src/rover_base_hal_lx16a.cpp
@@ -37,7 +37,9 @@
 #include "curio_base/rover_base_hal_lx16a.h"
 #include "curio_base/lx16a_encoder_filter_client.h"
 #include <serial/serial.h>
+#include <algorithm>
 #include <cmath>
+#include <string>
 
 namespace curio_base
 {
@@ -126,6 +128,35 @@ namespace curio_base
         return servo_pos;
     }
 
+    template <typename T>
+    bool validateServoParam(const T& param, const std::string& name, size_t expected_length)
+    {
+        if (param.size() != expected_length)
+        {
+            std::stringstream ss;
+            ss << "Parameter '" << name
+                << "' must be an array length " << expected_length
+                << ", got: " << param.size();
+            ROS_FATAL_STREAM(ss.str());
+            throw RoverBaseHALException(ss.str().c_str());
+            return false;
+        }
+        return true;
+    }
+
+    /// \brief simple case insensitive comparison
+    ///
+    /// See:
+    /// https://stackoverflow.com/questions/11635/case-insensitive-string-comparison-in-c
+    /// https://en.cppreference.com/w/cpp/string/byte/tolower
+    bool iequal(const std::string& a, const std::string& b)
+    {
+        return std::equal(a.begin(), a.end(), b.begin(), b.end(),
+            [](char a, char b) {
+                return tolower(static_cast<unsigned char>(a)) == tolower(static_cast<unsigned char>(b));
+            });
+    }    
+
     // Convert from velocity to servo duty.
     // int16_t velocity2ServoDuty(double velocity)
     // {
@@ -149,28 +180,107 @@ namespace curio_base
         steer_servo_orientations_(k_num_steers_),
         wheel_servo_duties_(k_num_wheels_, 0)
     {
-        // @TODO hardcoded parameters
-        // Parameters
-        wheel_servo_ids_ = { 11, 21, 12, 22, 13, 23 };
-        steer_servo_ids_ = { 111, 211, 131, 231 };
+        // Initialise parameters
+
+        // Wheel servo parameters
+        // wheel_servo_ids:           [     11,     12,      13,      21,      22,      23]
+        // wheel_servo_lon_labels:    ['front',  'mid',  'back', 'front',   'mid',  'back']
+        // wheel_servo_lat_labels:    [ 'left', 'left',  'left', 'right', 'right', 'right']
+        std::vector<int> wheel_servo_ids;
+        std::vector<std::string> wheel_servo_lon_labels;
+        std::vector<std::string> wheel_servo_lat_labels;
+        ros::param::get("~wheel_servo_ids", wheel_servo_ids);
+        ros::param::get("~wheel_servo_lon_labels", wheel_servo_lon_labels);
+        ros::param::get("~wheel_servo_lat_labels", wheel_servo_lat_labels);
+        validateServoParam(wheel_servo_ids, "wheel_servo_ids", k_num_wheels_);
+        validateServoParam(wheel_servo_lon_labels, "wheel_servo_lon_labels", k_num_wheels_);
+        validateServoParam(wheel_servo_lat_labels, "wheel_servo_lat_labels", k_num_wheels_);
+
+        // Steering servo parameters
+        // steer_servo_ids:           [    111,    131,     211,     231]
+        // steer_servo_lon_labels:    ['front', 'back', 'front',  'back']
+        // steer_servo_lat_labels:    [ 'left', 'left', 'right', 'right']
+        // steer_servo_angle_offsets: [    -10,     -5,      40,      20]
+        std::vector<int> steer_servo_ids;
+        std::vector<std::string> steer_servo_lon_labels;
+        std::vector<std::string> steer_servo_lat_labels;
+        std::vector<int> steer_servo_angle_offsets;
+        ros::param::get("~steer_servo_ids", steer_servo_ids);
+        ros::param::get("~steer_servo_lon_labels", steer_servo_lon_labels);
+        ros::param::get("~steer_servo_lat_labels", steer_servo_lat_labels);
+        ros::param::get("~steer_servo_angle_offsets", steer_servo_angle_offsets);
+        validateServoParam(steer_servo_ids, "steer_servo_ids", k_num_steers_);
+        validateServoParam(steer_servo_lon_labels, "steer_servo_lon_labels", k_num_steers_);
+        validateServoParam(steer_servo_lat_labels, "steer_servo_lat_labels", k_num_steers_);
+        validateServoParam(steer_servo_angle_offsets, "steer_servo_angle_offsets", k_num_steers_);
+
+        // wheel_servo_ids_ = { 11, 21, 12, 22, 13, 23 };
+        std::copy(wheel_servo_ids.begin(), wheel_servo_ids.end(), wheel_servo_ids_.begin());
+
+        // steer_servo_ids_ = { 111, 211, 131, 231 };
+        std::copy(steer_servo_ids.begin(), steer_servo_ids.end(), steer_servo_ids_.begin());
 
         // Right wheel servos aligned: -y axis => negative orientation
-        wheel_servo_orientations_ = { 1, -1, 1, -1, 1, -1 };
+        // wheel_servo_orientations_ = { 1, -1, 1, -1, 1, -1 };
+        for (size_t i=0; i<k_num_wheels_; ++i)
+        {
+            if (iequal(wheel_servo_lat_labels[i], "LEFT"))
+            {
+                wheel_servo_orientations_[i] = 1;
+            }
+            else if (iequal(wheel_servo_lat_labels[i], "RIGHT"))
+            {
+                wheel_servo_orientations_[i] = -1;
+            }
+            else
+            {
+                std::stringstream ss;
+                ss << "Invalid lateral laebl for wheel servo: "
+                    << wheel_servo_ids_[i]
+                    << ", got: " << wheel_servo_lat_labels[i]
+                    << " expect 'left' or 'right'";
+                ROS_FATAL_STREAM(ss.str());
+                throw RoverBaseHALException(ss.str().c_str());
+            }
+        }
 
         // Steer servos aligned: -z axis => negative orientation.
-        steer_servo_orientations_ = { -1, -1, -1, -1 };
+        // steer_servo_orientations_ = { -1, -1, -1, -1 };
+        for (size_t i=0; i<k_num_steers_; ++i)
+        {
+            steer_servo_orientations_[i] = -1;
+        }
 
-        // Parameters
-        
+        ROS_INFO_STREAM("Loading wheel servo parameters...");
+        for (size_t i=0; i<k_num_wheels_; ++i)
+        {
+            ROS_INFO_STREAM("id: " << wheel_servo_ids[i]
+                << ", lon: " << wheel_servo_lon_labels[i]
+                << ", lat: " << wheel_servo_lat_labels[i]
+                << ", orient: " << static_cast<int>(wheel_servo_orientations_[i]));
+        }
+
+        ROS_INFO_STREAM("Loading steer servo parameters...");
+        for (size_t i=0; i<k_num_steers_; ++i)
+        {
+            ROS_INFO_STREAM("id: " << steer_servo_ids[i]
+                << ", lon: " << steer_servo_lon_labels[i]
+                << ", lat: " << steer_servo_lat_labels[i]
+                << ", orient: " << static_cast<int>(steer_servo_orientations_[i])
+                << ", offset: " << steer_servo_angle_offsets[i]);
+        }
+
         // LX-16A servo driver - all parameters are required
-        ROS_INFO("Opening connection to servo bus board...");
+        // Servo driver port settings
+        // port: /dev/ttyACM0
+        // baudrate: 115200
+        // timeout: 1.0
         std::string port;
         int baudrate = 115200;
         double timeout = 1.0;           // [s]
         ros::param::get("~port", port);
         ros::param::get("~baudrate", baudrate);
         ros::param::get("~timeout", timeout);
-        uint32_t timeout_millis = static_cast<uint32_t>(timeout * 1000);
 
         // Encoder filters
         std::string classifier_filename;
@@ -179,19 +289,19 @@ namespace curio_base
         ros::param::get("~classifier_window", classifier_window);
         if (!ros::param::has("~classifier_filename"))
         {
-            ROS_FATAL("Missing parameter: classifier_filename...calling ros shutdown");
-            ros::shutdown();
+            ROS_FATAL("Missing parameter: classifier_filename");
         }
         ros::param::get("~classifier_filename", classifier_filename);
 
         if (!ros::param::has("~regressor_filename"))
         {
-            ROS_FATAL("Missing parameter: regressor_filename...calling ros shutdown");
-            ros::shutdown();
+            ROS_FATAL("Missing parameter: regressor_filename");
         }
         ros::param::get("~regressor_filename", regressor_filename );
 
         // Initialise servo driver.
+        ROS_INFO("Initialising LX16A servo driver...");
+        uint32_t timeout_millis = static_cast<uint32_t>(timeout * 1000);
         serial::Timeout serial_timeout = serial::Timeout::simpleTimeout(timeout_millis);
         ros::Duration response_timeout(0.015);
         servo_driver_ = std::unique_ptr<LX16ADriver>(new LX16ADriver());
@@ -205,9 +315,7 @@ namespace curio_base
         }
         catch(const serial::IOException & e)
         {
-            ROS_FATAL_STREAM("LX16A driver: failed to open port: "
-                << port << "...calling ros shutdown");
-            ros::shutdown();
+            ROS_FATAL_STREAM("LX16A driver: failed to open port: " << port);
         }        
         ROS_INFO_STREAM("LX16A driver: port: " << servo_driver_->getPort());
         ROS_INFO_STREAM("LX16A driver: baudrate: " << servo_driver_->getBaudrate());
@@ -219,6 +327,7 @@ namespace curio_base
         ros::Duration(1.0).sleep();
 
         // Initialise encoder filter.
+        ROS_INFO("Initialising LX16A encoder filters...");
         std::unique_ptr<LX16AEncoderFilterClient> filter(
             new LX16AEncoderFilterClient(
                 nh, classifier_filename, regressor_filename, classifier_window));
@@ -228,10 +337,17 @@ namespace curio_base
         ROS_INFO_STREAM("LX16A encoder: regressor_filename: " << regressor_filename);
         ROS_INFO_STREAM("LX16A encoder: classifier_window: " << classifier_window);
         
-        // Add servos
+        // Add servos to encoder filter
         for (size_t i=0; i<k_num_wheels_; ++i)
         {
             encoder_filter_->add(wheel_servo_ids_[i]);
+        }
+
+        // Initialise steering trim (offsets)
+        ROS_INFO_STREAM("Setting steer trim...");
+        for (size_t i=0; i<k_num_steers_; ++i)
+        {
+            setSteerTrim(i, static_cast<int16_t>(steer_servo_angle_offsets[i]));
         }
     }
 
@@ -356,5 +472,25 @@ namespace curio_base
                 << " set steer: id: " << static_cast<int>(servo_id));
         }
     }
+
+    /// \brief Set the steering trim for the i-ith steer.
+    void RoverBaseHALLX16A::setSteerTrim(int i, int16_t offset)
+    {
+        uint8_t servo_id = steer_servo_ids_[i];
+        int8_t orientation = steer_servo_orientations_[i];
+
+        try {
+            servo_driver_->setPositionOffset(servo_id, offset);
+            servo_driver_->savePositionOffset(servo_id);
+
+            ROS_DEBUG_STREAM("set steer: id: " << static_cast<int>(servo_id)
+                << ", offset: " << static_cast<int>(offset));
+        }
+        catch(const LX16AException &e) {
+            ROS_ERROR_STREAM(e.what()
+                << " set steer: id: " << static_cast<int>(servo_id));
+        }
+    }
+
 
 } // namespace curio_base
