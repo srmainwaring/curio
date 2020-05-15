201430e0a07392b6f81b99500e53ab225344886f
Rhys Mainwaring <rhys.mainwaring@me.com>
2020-05-15 09:52:14 +0100

    Add utility to add command line arguments to an embedded python interpreter.
    1. Add utility to ensure command line args are set on the 'sys' module for embedded interpreters.
    2. Remove duplicate documentation in derived classes and replace with \copydocs directives.

--- lx16a/include/lx16a/lx16a_encoder_filter_client.h
+++ lx16a/include/lx16a/lx16a_encoder_filter_client.h
@@ -46,11 +46,11 @@
 
 namespace lx16a
 {
-    /// \brief An encoder filter for the LX-16A servo.
+    /// \copydoc LX16AEncoderFilter
     class LX16AEncoderFilterClient : public LX16AEncoderFilter
     {
     public:
-        /// \brief Destructor
+        /// \copydoc LX16AEncoderFilter::~LX16AEncoderFilter()
         virtual ~LX16AEncoderFilterClient();
 
         /// \brief Constructor
@@ -69,91 +69,45 @@ namespace lx16a
             const std::string &regressor_filename = "",
             int16_t window = 10);
 
-        /// \brief Initialise the encoder filter.
+        /// \copydoc LX16AEncoderFilter::init()
         void init() override;
 
-        /// \brief Add a servo.
+        /// \copydoc LX16AEncoderFilter::add()
         void add(uint8_t servo_id) override;
 
-        /// \brief Update the encoder filter.
-        ///
-        /// Update the encoder and estimate whether or not the new servo
-        /// position is in the valid range.
-        ///
-        /// The feature vector X contains 3 * window entries:
-        ///     dt[window]     the change in ros_time between servo
-        ///                    position readings
-        ///     duty[window]   the commanded duty to the LX-16A
-        ///     pos[window]    the measured position on the LX-16A
-        ///        
-        /// \param ros_time A ros::Time containing the time from ros::Time::now()
-        /// \param duty An integer servo duty
-        /// \param position An integer servo position        
-        ///
+        /// \copydoc LX16AEncoderFilter::update()
         void update(uint8_t servo_id, const ros::Time &ros_time, int16_t duty, int16_t position) override;
 
-        /// \brief Get the number of revoutions since reset.
-        ///
-        /// \return An integer number of revolutions since the count was reset.
-        ///
+        /// \copydoc LX16AEncoderFilter::getRevolutions()
         int16_t getRevolutions(uint8_t servo_id) const override;
 
-        /// \brief Get the current encoder count since reset (filtered).
-        ///
-        /// Note that the encoder count is offset from the servo position
-        /// so that the count is zero when the encoder filter is reset.
-        ///
-        /// \return An integer, the current encoder count.
-        ///
+        /// \copydoc LX16AEncoderFilter::getCount()
         int16_t getCount(uint8_t servo_id) const override;
 
-        /// \brief Get the current encoder duty.
-        ///
-        /// \return An integer, the current encoder duty.
-        ///
+        /// \copydoc LX16AEncoderFilter::getDuty()
         int16_t getDuty(uint8_t servo_id) const override;
 
-        /// Get the angular position of the encoder (filtered)
-        ///
-        /// \return A double, the angular position of the encoder [rad].
-        ///
+        /// \copydoc LX16AEncoderFilter::getAngularPosition()
         double getAngularPosition(uint8_t servo_id) const override;
 
-        /// \brief Get the current (un-filtered) servo position and an
-        /// estimate whether it is valid.
-        ///
-        /// \param position An integer reference that is set to the current position. 
-        /// \param is_valid A bool that is set to true for valid, false otherwise.
-        /// \param map_position A bool: if true map the position to the
-        /// range [0, 1500],  has (default True)
-        ///
+        /// \copydoc LX16AEncoderFilter::getServoPosition()
         void getServoPosition(uint8_t servo_id, int16_t &position, bool &is_valid, bool map_position=true) const override;
 
-        /// \brief Get the invert state: whether the encoder count is inverted.
-        ///
-        /// \return An integer: -1 if the count is inverted, 1 otherwise. 
-        ///
+        /// \copydoc LX16AEncoderFilter::getInvert()
         int16_t getInvert(uint8_t servo_id) const override;
 
-        /// \brief Invert the direction of the encoder count.
-        ///
-        /// \param is_inverted A bool: true if the encoder count is reversed.
-        ///
+        /// \copydoc LX16AEncoderFilter::setInvert()
         void setInvert(uint8_t servo_id, bool is_inverted) override;
 
-        /// Reset the encoder counters to zero.
-        ///
-        /// \param position An integer (assumed valid) position of the
-        /// servo when the encoder is reset.  
-        /// 
+        /// \copydoc LX16AEncoderFilter::reset()
         void reset(uint8_t servo_id, int16_t position) override;
 
         /// Vectorised interface
 
-        /// \brief Add servos.
+        /// \copydoc LX16AEncoderFilter::add_v()
         void add_v(const std::vector<uint8_t> &servo_ids) override;
 
-        /// \brief Update the encoder filter.
+        /// \copydoc LX16AEncoderFilter::update_v()
         void update_v(
             const std::vector<uint8_t> &servo_ids,
             const ros::Time &ros_time,
--- lx16a/include/lx16a/lx16a_encoder_filter_onnx.h
+++ lx16a/include/lx16a/lx16a_encoder_filter_onnx.h
@@ -51,11 +51,11 @@ namespace lx16a
 {
     class LX16AEncoderFilterOnnxImpl;
 
-    /// \brief An encoder filter for the LX-16A servo.
+    /// \copydoc LX16AEncoderFilter.
     class LX16AEncoderFilterOnnx : public LX16AEncoderFilter
     {
     public:
-        /// \brief Destructor
+        /// \copydoc LX16AEncoderFilter::~LX16AEncoderFilter()
         virtual ~LX16AEncoderFilterOnnx();
 
         /// \brief Constructor
@@ -73,107 +73,51 @@ namespace lx16a
             const std::string &regressor_filename = "",
             int16_t window = 10);
 
-        /// \brief Initialise the encoder filter.
+        /// \copydoc LX16AEncoderFilter::init()
         void init() override;
 
-        /// \brief Add a servo.
+        /// \copydoc LX16AEncoderFilter::add()
         void add(uint8_t servo_id) override;
 
-        /// \brief Update the encoder filter.
-        ///
-        /// Update the encoder and estimate whether or not the new servo
-        /// position is in the valid range.
-        ///
-        /// The feature vector X contains 3 * window entries:
-        ///     dt[window]     the change in ros_time between servo
-        ///                    position readings
-        ///     duty[window]   the commanded duty to the LX-16A
-        ///     pos[window]    the measured position on the LX-16A
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \param ros_time A ros::Time containing the time from ros::Time::now()
-        /// \param duty An integer servo duty
-        /// \param position An integer servo position        
-        ///
+        /// \copydoc LX16AEncoderFilter::update()
         void update(uint8_t servo_id, const ros::Time &ros_time, int16_t duty, int16_t position) override;
 
-        /// \brief Get the number of revoutions since reset.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \return An integer number of revolutions since the count was reset.
-        ///
+        /// \copydoc LX16AEncoderFilter::getRevolutions()
         int16_t getRevolutions(uint8_t servo_id) const override;
 
-        /// \brief Get the current encoder count since reset (filtered).
-        ///
-        /// Note that the encoder count is offset from the servo position
-        /// so that the count is zero when the encoder filter is reset.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \return An integer, the current encoder count.
-        ///
+        /// \copydoc LX16AEncoderFilter::getCount()
         int16_t getCount(uint8_t servo_id) const override;
 
-        /// \brief Get the current encoder duty.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \return An integer, the current encoder duty.
-        ///
+        /// \copydoc LX16AEncoderFilter::getDuty()
         int16_t getDuty(uint8_t servo_id) const override;
 
-        /// Get the angular position of the encoder (filtered)
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \return A double, the angular position of the encoder [rad].
-        ///
+        /// \copydoc LX16AEncoderFilter::getAngularPosition()
         double getAngularPosition(uint8_t servo_id) const override;
 
-        /// \brief Get the current (un-filtered) servo position and an
-        /// estimate whether it is valid.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \param position An integer reference that is set to the current position. 
-        /// \param is_valid A bool that is set to true for valid, false otherwise.
-        /// \param map_position A bool: if true map the position to the
-        /// range [0, 1500],  has (default True)
-        ///
+        /// \copydoc LX16AEncoderFilter::getServoPosition()
         void getServoPosition(uint8_t servo_id, int16_t &position, bool &is_valid, bool map_position=true) const override;
 
-        /// \brief Get the invert state: whether the encoder count is inverted.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \return An integer: -1 if the count is inverted, 1 otherwise. 
-        ///
+        /// \copydoc LX16AEncoderFilter::getInvert()
         int16_t getInvert(uint8_t servo_id) const override;
 
-        /// \brief Invert the direction of the encoder count.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \param is_inverted A bool: true if the encoder count is reversed.
-        ///
+        /// \copydoc LX16AEncoderFilter::setInvert()
         void setInvert(uint8_t servo_id, bool is_inverted) override;
 
-        /// Reset the encoder counters to zero.
-        ///
-        /// \param servo_id An integer for servo serial identifier, in [0, 253]
-        /// \param position An integer (assumed valid) position of the
-        /// servo when the encoder is reset.  
-        /// 
+        /// \copydoc LX16AEncoderFilter::reset()
         void reset(uint8_t servo_id, int16_t position) override;
 
         /// Vectorised interface
 
-        /// \brief Add servos.
+        /// \copydoc LX16AEncoderFilter::add_v()
         void add_v(const std::vector<uint8_t> &servo_ids) override;
 
-        /// \brief Update the encoder filter.
+        /// \copydoc LX16AEncoderFilter::update_v()
         void update_v(
             const std::vector<uint8_t> &servo_ids,
             const ros::Time &ros_time,
             const std::vector<int16_t> &duties,
             const std::vector<int16_t> &positions,
             std::vector<double> &angular_positions) override;
-
     };
 
 } // namespace lx16a
